c80fd6756776d5ecad28f459e9ed1c94163bbc38
lagudu-glitch <79684098+lagudu-glitch@users.noreply.github.com>
2023-06-11 17:35:55 -0400

    Removed sys and fixed source

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import subprocess, sys
+import subprocess
 
 if __name__ == "__main__":
     subprocess.call ("mkdir -p ~/pdk", shell=True)
     subprocess.call ("mv ./lib/ ~/pdk", shell=True)
     subprocess.call ("mv ./verilog/ ~/pdk", shell=True)
     subprocess.call ('echo "export PDK_ROOT=~/pdk" >> ~/.bashrc', shell=True)
-    subprocess.call ("source ~/.bashrc", shell=True)
+    subprocess.call ('echo "source ~/.bashrc" >> ~/.bash_profile', shell=True)
