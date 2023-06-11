6406450e144622752c2285471088992d3c3b5d80
lagudu-glitch <79684098+lagudu-glitch@users.noreply.github.com>
2023-06-11 18:32:06 -0400

    Changed root name

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.call ("mkdir -p ~/pdk", shell=True)
-    subprocess.call ("mv ./lib/ ~/pdk", shell=True)
-    subprocess.call ("mv ./verilog/ ~/pdk", shell=True)
-    subprocess.call ('echo "export PDK_ROOT=~/pdk" >> ~/.bashrc', shell=True)
+    subprocess.call ("mkdir -p ~/sky130A", shell=True)
+    subprocess.call ("mv ./lib/ ~/sky130A", shell=True)
+    subprocess.call ("mv ./verilog/ ~/sky130A", shell=True)
+    subprocess.call ('echo "export PDK_ROOT=~/sky130A" >> ~/.bashrc', shell=True)
     subprocess.call ('echo "source ~/.bashrc" >> ~/.bash_profile', shell=True)
