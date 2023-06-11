63767942b6f60e3e3dda3ac3c646e83b61ed68c5
lagudu-glitch <79684098+lagudu-glitch@users.noreply.github.com>
2023-06-11 17:19:05 -0400

    Changed Structure

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import subprocess, sys
 
 if __name__ == "__main__":
-    subprocess.call ("mkdir -p ~/material", shell=True)
-    subprocess.call ("mv ./pdk/ ~/material", shell=True)
-    subprocess.call ("mv Makefile ~/material", shell=True)
-    subprocess.call ('echo "export PDK_ROOT=~/material/pdk" >> ~/.bashrc', shell=True)
-    subprocess.call ("source .zshrc", shell=True)
+    subprocess.call ("mkdir -p ~/pdk", shell=True)
+    subprocess.call ("mv ./lib/ ~/pdk", shell=True)
+    subprocess.call ("mv ./verilog/ ~/pdk", shell=True)
+    subprocess.call ('echo "export PDK_ROOT=~/pdk" >> ~/.bashrc', shell=True)
+    subprocess.call ("source ~/.bashrc", shell=True)
