1811827e3efe2c0f364e6b06c7913fb01b533060
lagudu-glitch <79684098+lagudu-glitch@users.noreply.github.com>
2023-06-11 21:31:27 -0400

    remove source

--- setup.py
+++ setup.py
@@ -7,5 +7,3 @@ if __name__ == "__main__":
     subprocess.call ("mv ./lib/ ~/sky130A", shell=True)
     subprocess.call ("mv ./verilog/ ~/sky130A", shell=True)
     subprocess.call ('echo "export PDK_ROOT=~/sky130A" >> ~/.bashrc', shell=True)
-    subprocess.call ('echo "source ~/.bashrc" >> ~/.bash_profile', shell=True)
-    os.system("source ~/.bashrc)
