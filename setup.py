84a491ae2f8542ec4d06ab195003a19b2cc1a672
lagudu-glitch <79684098+lagudu-glitch@users.noreply.github.com>
2023-06-11 23:54:25 -0400

    Newline

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     subprocess.call ("mkdir -p ~/sky130A", shell=True)
     subprocess.call ("mv ./lib/ ~/sky130A", shell=True)
     subprocess.call ("mv ./verilog/ ~/sky130A", shell=True)
-    subprocess.call ('echo "export PDK_ROOT=~/sky130A" >> ~/.bashrc', shell=True)
+    subprocess.call ('echo "\nexport PDK_ROOT=~/sky130A" >> ~/.bashrc', shell=True)
